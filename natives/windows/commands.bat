20aa923f705da5ece3134500430d8a0684569191
HuanFran <53756287+HuanFran@users.noreply.github.com>
2021-09-26 15:09:53 +1000

    .

--- natives/windows/commands.bat
+++ natives/windows/commands.bat
@@ -1,1 +1,8 @@
-gcc -shared src/main/c/common/commands.c -o natives/windows/commands.dll -Iinclude/windows/jni -Iinclude/windows/vulkan -Llib/windows -lvulkan-1 -D VK_USE_PLATFORM_WIN32_KHR
+gcc -shared src\main\c\common\commands.c ^
+ -o natives\windows\commands.dll ^
+ -I "%JAVA_HOME%\include" ^
+ -I "%JAVA_HOME%\include\win32" ^
+ -I "%VULKAN_SDK%\include\vulkan" ^
+ -L "%VULKAN_SDK%\lib" ^
+ -l "vulkan-1" ^
+ -D VK_USE_PLATFORM_WIN32_KHR ^
--- natives/windows/native_sizes.bat
+++ natives/windows/native_sizes.bat
@@ -1,1 +1,4 @@
-gcc -shared src/main/c/common/native_sizes.c -o natives/windows/native_sizes.dll -Iinclude/windows/jni
+gcc -shared src/main/c/common/native_sizes.c ^
+ -o "natives/windows/native_sizes.dll" ^
+ -I "%JAVA_HOME%\include" ^
+ -I "%JAVA_HOME%\include\win32"
